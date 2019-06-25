e8e45b320c66a80529394c479b1f4660314e7713
Can <layso@Cans-iMac.local>
2019-06-25 01:22:56 -0700

    Project files created

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Can on 25.06.2019.
+//  Copyright © 2019 layso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
