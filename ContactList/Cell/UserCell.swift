254d6ce19f145fb96f3da8ceeafad95e8388cc72
Can <layso@Cans-iMac.local>
2019-06-25 19:13:34 -0700

    Storyboard design and constraints are done

--- ContactList/Cell/UserCell.swift
+++ ContactList/Cell/UserCell.swift
@@ -10,15 +10,8 @@ import UIKit
 
 class UserCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var mail: UILabel!
 }
