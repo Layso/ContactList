8c09dff6186c0a6e941a66d05698e4662e99a069
Can <layso@Cans-iMac.local>
2019-06-25 19:13:22 -0700

    Storyboard design and constraints are done

---
+++ ContactList/Cell/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  ContactList
+//
+//  Created by Can on 25.06.2019.
+//  Copyright © 2019 layso. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
